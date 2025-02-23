eba7cd626362b7b51f61b1ff59d09101d5de1ef8
MukhammadZiya <mukhammadziyaa@gmail.com>
2025-02-23 14:16:40 +0900

    feat: integration of .env variables

--- apps/travelingo-batch/src/main.ts
+++ apps/travelingo-batch/src/main.ts
@@ -3,6 +3,6 @@ import { TravelingoBatchModule } from './travelingo-batch.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(TravelingoBatchModule);
-  await app.listen(process.env.port ?? 3000);
+  await app.listen(process.env.PORT_BATCH ?? 3000);
 }
 bootstrap();
--- apps/travelingo-batch/src/travelingo-batch.module.ts
+++ apps/travelingo-batch/src/travelingo-batch.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { TravelingoBatchController } from './travelingo-batch.controller';
 import { TravelingoBatchService } from './travelingo-batch.service';
+import { ConfigModule } from '@nestjs/config';
+
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot()],
   controllers: [TravelingoBatchController],
   providers: [TravelingoBatchService],
 })
