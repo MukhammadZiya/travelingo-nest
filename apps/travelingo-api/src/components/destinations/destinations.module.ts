45f3aa912dc45cad964a707b40dd8034adea88a6
MukhammadZiya <mukhammadziyaa@gmail.com>
2025-02-24 22:42:58 +0900

    feat: develop member resolver and service

--- apps/travelingo-api/src/components/destinations/destinations.module.ts
+++ apps/travelingo-api/src/components/destinations/destinations.module.ts
@@ -1,4 +1,8 @@
 import { Module } from '@nestjs/common';
+import { DestinationsResolver } from './destinations.resolver';
+import { DestinationsService } from './destinations.service';
 
-@Module({})
+@Module({
+  providers: [DestinationsResolver, DestinationsService]
+})
 export class DestinationsModule {}
