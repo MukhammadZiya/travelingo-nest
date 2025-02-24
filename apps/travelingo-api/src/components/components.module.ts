70eed1b2697a2f2bd45b4bc35d73314ae2bdf6e7
MukhammadZiya <mukhammadziyaa@gmail.com>
2025-02-24 21:30:52 +0900

    feat: open component related modules

--- apps/travelingo-api/src/components/components.module.ts
+++ apps/travelingo-api/src/components/components.module.ts
@@ -1,8 +1,14 @@
 import { Module } from '@nestjs/common';
 import { MemberModule } from './member/member.module';
 import { DestinationsModule } from './destinations/destinations.module';
+import { AuthModule } from './auth/auth.module';
+import { CommentsModule } from './comments/comments.module';
+import { LikeModule } from './like/like.module';
+import { ViewModule } from './view/view.module';
+import { FollowModule } from './follow/follow.module';
+import { BoardArticleModule } from './board-article/board-article.module';
 
 @Module({
-  imports: [MemberModule, DestinationsModule]
+  imports: [MemberModule, DestinationsModule, AuthModule, CommentsModule, LikeModule, ViewModule, FollowModule, BoardArticleModule]
 })
 export class ComponentsModule {}
